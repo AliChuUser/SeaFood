d2fa642b705a50b9b8cd427aa0b41998f73632ff
Aleksei Chudin <alekseichudin@macbook-air-alichu.local>
2019-03-16 13:33:42 +0300

    Initial Commit

---
+++ SeaFood/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SeaFood
+//
+//  Created by Aleksei Chudin on 16/03/2019.
+//  Copyright © 2019 Aleksei Chudin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
