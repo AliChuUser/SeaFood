468f211ae7852cfce29695b54397f3a6d4fa9ed0
AliChuUser <pavdm89@mail.ru>
2019-05-29 02:32:11 +0300

    Update commit

--- SeaFood/ViewController.swift
+++ SeaFood/ViewController.swift
@@ -7,14 +7,73 @@
 //
 
 import UIKit
+import CoreML
+import Vision
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    let imagePicker = UIImagePickerController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        imagePicker.delegate = self
+        imagePicker.sourceType = .photoLibrary
+        imagePicker.allowsEditing = false
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        guard let userPickedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage else {
+            fatalError("User Picked image is nil")
+        }
+        
+        imageView.image = userPickedImage
+            
+        guard let ciimage = CIImage(image: userPickedImage) else {
+                fatalError("Could not convert user image (UIImage) to CIImage")
+        }
+        
+        detect(image: ciimage)
+        
+        imagePicker.dismiss(animated: true, completion: nil)
+    }
+    
+    func detect(image: CIImage) {
+        
+        guard let model = try? VNCoreMLModel(for: Inceptionv3().model) else {
+            fatalError("Loading CoreML Model failed")
+        }
+        
+        let request = VNCoreMLRequest(model: model) { (request, error) in
+            guard let results = request.results as? [VNClassificationObservation] else {
+                fatalError("Model failed to process image")
+            }
+            
+            if let firstResult = results.first {
+                if firstResult.identifier.contains("hotdog") {
+                    self.navigationItem.title = "It`s hotdog!"
+                } else {
+                    self.navigationItem.title = "It`s not hotdog"
+                }
+            }
+        }
+        
+        let handler = VNImageRequestHandler(ciImage: image)
+        
+        do {
+            try handler.perform([request])
+        } catch {
+            print(error)
+        }
     }
 
-
+    @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
+        
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
 }
 
